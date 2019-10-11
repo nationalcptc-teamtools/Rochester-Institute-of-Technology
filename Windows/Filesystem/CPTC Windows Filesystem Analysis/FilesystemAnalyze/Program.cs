7ae7987d14d29036b02a3e907274f7a6297897f1
D4stiny <bd6463@rit.edu>
2019-10-11 00:43:25 -0400

    Update

---
+++ Windows/Filesystem/CPTC Windows Filesystem Analysis/FilesystemAnalyze/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FilesystemAnalyze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Usage:");
+                Console.WriteLine("FilesystemAnalyze.exe [BASELINE] [LIVE MACHINE] [OUTPUT]");
+                Console.WriteLine("\tBASELINE = The path to the baseline file.");
+                Console.WriteLine("\tLIVE MACHINE = The path to the results of a live machine.");
+                Console.WriteLine("\tOUTPUT = Filename for results.");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("[FilesystemAnalyze] Reading in baseline file " + args[0] + ".");
+            string[] importedBaselineFiles = File.ReadAllLines(args[0]);
+
+            Console.WriteLine("[FilesystemAnalyze] Sorting in baseline file " + args[0] + ".");
+            Array.Sort(importedBaselineFiles, StringComparer.InvariantCulture);
+
+            Console.WriteLine("[FilesystemAnalyze] Reading in live machine file " + args[1] + ".");
+            string[] importedLiveFiles = File.ReadAllLines(args[1]);
+
+            Console.WriteLine("[FilesystemAnalyze] Beginning single-threaded search, results will be stored at " + args[2] + ".");
+            StreamWriter outputFile = File.AppendText(args[2]);
+
+            foreach(string livepath in importedLiveFiles)
+            {
+                if(Array.BinarySearch(importedBaselineFiles, Regex.Replace(livepath, @"[^\u0000-\u007F]+", string.Empty)) < 0)
+                {
+                    outputFile.WriteLine(livepath);
+                }
+            }
+
+            outputFile.Close();
+
+            Console.WriteLine("[FilesystemAnalyze] Finished dumping.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Windows/Filesystem/CPTC Windows Filesystem Analysis/FilesystemGen/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FilesystemGen
+{
+    class Program
+    {
+        static void DumpDirectory(StreamWriter outputFile, string path)
+        {
+            try
+            {
+                string[] directories = Directory.GetDirectories(path);
+                string[] files = Directory.GetFiles(path);
+
+                foreach (string directory in directories)
+                {
+                    DumpDirectory(outputFile, directory);
+                }
+
+                foreach (string file in files)
+                {
+                    outputFile.WriteLine(Regex.Replace(file, @"[^\u0000-\u007F]+", string.Empty));
+                }
+            } catch (Exception e)
+            {
+                Console.WriteLine("Swallowed exception " + e.ToString());
+            }
+        }
+        static void Main(string[] args)
+        {
+            if(args.Length < 2)
+            {
+                Console.WriteLine("Usage:");
+                Console.WriteLine("FilesystemGen.exe [BASE PATH] [OUTPUT]");
+                Console.WriteLine("\tBASE PATH = The path to start mapping at.");
+                Console.WriteLine("\tOUTPUT = The path to the output logs.");
+                Console.ReadLine();
+                return;
+            }
+
+            string basepath = args[0];
+            string outputFileName = args[1];
+
+            Console.WriteLine("[FilesystemGen] Dumping path " + basepath + ".");
+
+            StreamWriter outputFile = File.AppendText(outputFileName);
+            DumpDirectory(outputFile, basepath);
+            outputFile.Close();
+
+            Console.WriteLine("[FilesystemGen] Dumped to " + outputFileName + ".");
+            Console.ReadLine();
+        }
+    }
+}
